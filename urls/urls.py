3f37051773ebcec5e05da0a51e123f0f31301889
mstislav <strong13strong@gmail.com>
2022-01-13 17:21:35 +0700

    commit lesson_4

--- urls/urls.py
+++ urls/urls.py
@@ -1,10 +1,16 @@
 from datetime import date
-from views.views import Index, About, Contacts
+from views.views import Index, About, Contacts, MyCourseList, \
+    CreateMyCourse, CategoryCreator, MyCategoryList, CourseCopy
 
 routes = {
     '/': Index(),
     '/about/': About(),
     '/contacts/': Contacts(),
+    '/create_my_course/': CreateMyCourse(),
+    '/category_create/': CategoryCreator(),
+    '/my_course_list/': MyCourseList(),
+    '/my_category_list/': MyCategoryList(),
+    '/course_copy/': CourseCopy(),    
 }
 
 # front controller(Контроллер запросов)
